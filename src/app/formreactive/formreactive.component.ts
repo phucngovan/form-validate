30d12d77a6f60a2d83acc2caae9e048d4ac4ba65
phucngovan <phucdepzai0712@gmail.com>
2019-03-21 12:22:59 +0700

    fix loi ma bann html

--- src/app/formreactive/formreactive.component.ts
+++ src/app/formreactive/formreactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {MustMatch} from '../must-match/must-match.validator';
 
 @Component({
   selector: 'app-formreactive',
@@ -7,33 +8,37 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./formreactive.component.css']
 })
 export class FormreactiveComponent implements OnInit {
-    form = new FormGroup({
-      firstName: new FormControl('', Validators.required),
-      lastName: new FormControl('', Validators.required),
-      email: new FormControl('', [
-        Validators.required,
-        Validators.pattern('(\\W|^)[\\w.+\\-]*@gmail\\.com(\\W|$)')
-      ]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6)
-      ])
-  });
-  // form: FormGroup;
+//     form = new FormGroup({
+//       firstName: new FormControl('', Validators.required),
+//       lastName: new FormControl('', Validators.required),
+//       email: new FormControl('', [
+//         Validators.required,
+//         Validators.pattern('(\\W|^)[\\w.+\\-]*@gmail\\.com(\\W|$)')
+//       ]),
+//       password: new FormControl('', [
+//         Validators.required,
+//         Validators.minLength(6)
+//       ]),
+//       confirmPassword: new FormControl('' , Validators.required)
+//   }, {
+//       validator: MustMatch('password', 'confirmPassword')
+// });
+  form: FormGroup;
+  // convenience getter for easy access to form fields
+  get f() { return this.form.controls; }
 
   constructor(private formBuilder: FormBuilder) { }
 
-  get firstname() {
-    return this.form.get('firstName');
-  }
-
   ngOnInit() {
-    // this.form = this.formBuilder.group({
-    //   firstName: ['', Validators.required],
-    //   lastName: ['', Validators.required],
-    //   email: ['', Validators.required, Validators.email],
-    //   password: ['', Validators.required, Validators.minLength(6)]
-    // });
+    this.form = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern('(\\W|^)[\\w.+\\-]*@gmail\\.com(\\W|$)')]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    } , {
+      validator: MustMatch('password', 'confirmPassword')
+    });
   }
   onSubmit() {
     alert(JSON.stringify(this.form.value));
